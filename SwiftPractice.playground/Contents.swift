9d2f97102c403a969e53dd71cde31df0e8e08bf3
NurdauletNAGIBATOR <nurdaulet.kadyrov05@gmail.com>
2026-09-11 18:32:49 +0500

    first hw

---
+++ SwiftPractice.playground/Contents.swift
@@ -0,0 +1,20 @@
+let firstName = "Nurdaulet"
+let lastName = "Kalybai"
+let birthYear = 2006
+let isStudent = true
+let height = 1.72
+
+let university = "KBTU"
+let hobby = "sleeping"
+let numberOfHobbies = 2
+let favoriteNumber = 10
+let isHobbyCreative = true
+
+let currentYear = 2026
+let age = currentYear - birthYear
+
+let futureGoals = "become a happy succesfull person"
+
+let lifeStory = "My name is \(firstName) \(lastName). I am \(age) years old, born in \(birthYear). I am currently a student: \(isStudent). I study at \(university). My height is \(height) meters. I enjoy \(hobby), which is a creative hobby: \(isHobbyCreative). I have \(numberOfHobbies) hobbies in total, and my favorite number is \(favoriteNumber). In the future, I want to \(futureGoals)."
+
+print(lifeStory)
